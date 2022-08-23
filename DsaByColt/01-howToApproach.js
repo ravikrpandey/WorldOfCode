624e4b8d0c3f969837093f03af555c28c7830a9d
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-08-24 01:59:07 +0530

    countCharector

--- DsaByColt/01-howToApproach.js
+++ DsaByColt/01-howToApproach.js
@@ -1,24 +1,52 @@
 // Write a function which takes a string and returns counts of each character in the string.
 
-function countCharacter(str) {
-  console.log(str);
+// function countCharacter(str) {
+//   console.log(str);
 
-  // make a variable to return at the end
-  let result = {};
+//   // make a variable to return at the end
+//   let result = {};
 
-  for (let i = 0; i < str.length; i++) {
-    var char = str[i];
+//   for (let i = 0; i < str.length; i++) {
+//     var char = str[i];
 
-    if (result[char > 0]) {
-      result[char]++;
-    } else {
-      result[char] = 1;
-    };
+//     if (result[char > 0]) {
+//       result[char]++;
+//     } else {
+//       result[char] = 1;
+//     };
 
     
-  }
-return result
+//   }
+// return result
   
+// }
+
+// console.log(countCharacter("ravi"));
+
+//--------------------------------------------or------------------------------------------
+
+function charCount(str) {
+
+    let obj = {}
+
+    for(let char of str) {
+        if(isAlphaNumaric(char)) {
+            char = char.toLowerCase();
+            obj[char] = ++obj[char] || 1
+        }
+
+    }
+    return obj
 }
 
-console.log(countCharacter("ravi"));
+function isAlphaNumaric(char) {
+    let code = char.charCodeAt(0);
+    if((!code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)) {
+        return false
+    }
+    return true
+}
+
+console.log(charCount("Ravi is a good boy 1234"))
