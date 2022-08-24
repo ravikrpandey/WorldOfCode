0840742e93905910d84abd23d72c59de52ad560b
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-08-24 15:52:25 +0530

    updated

--- GroupCode/findDuplicateElement.js
+++ GroupCode/findDuplicateElement.js
@@ -3,11 +3,13 @@
 
 let arr = [1, 1, 2, 4, 5, 6, 2];
 let len = arr.length
+let temp = []
 
 function duplicateElement(arr) {
     for(let i=0; i<len; i++) {
        
             if(arr[i] === arr[i+1]) {
+
                 console.log(arr[i])
             }
         }
