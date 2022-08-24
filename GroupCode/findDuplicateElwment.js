d7993104c56a75d4a66d69ca2c650510576133b7
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-08-24 15:39:02 +0530

    updated

---
+++ GroupCode/findDuplicateElwment.js
@@ -0,0 +1,17 @@
+
+// Find th duplicate element in the array
+
+let arr = [1, 1, 2, 4, 5, 6, 2];
+let len = arr.length
+
+function duplicateElement(arr) {
+    for(let i=0; i<len; i++) {
+       
+
+            if(arr[i] === arr[i+1]) {
+                console.log(arr[i])
+            }
+        }
+    }
+
+duplicateElement(arr)
