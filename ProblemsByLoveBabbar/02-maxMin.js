a1c1e986113e6a8e7e17d624a56f78bb32280527
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-08-26 01:18:05 +0530

    getMaxMin

---
+++ ProblemsByLoveBabbar/02-maxMin.js
@@ -0,0 +1,56 @@
+
+// find the maximum and minimum element from the array
+
+// Given an array A of size N of integers. Your task is to find the sum of minimum and maximum elements in the array.
+
+// Example 1:
+
+// Input:
+// N = 5
+// A[] = {-2, 1, -4, 5, 3}
+// Output: 1
+// Explanation: min = -4, max =  5. Sum = -4 + 5 = 1
+
+let arr = [1000, 11, 445, 1, 330, 3000];
+var arr_size = 6;
+
+
+function getMinMax(arr, n) {
+    
+    
+        let minmax = new  Array();
+        var i;
+        var min;
+        var max;
+ 
+        /*If there is only one element then return it as min and max both*/
+        if (n == 1) {
+            minmax.max = arr[0];
+            minmax.min = arr[0];
+            return minmax;
+        }
+ 
+        /* If there are more than one elements, then initialize min
+    and max*/
+        if (arr[0] > arr[1]) {
+            minmax.max = arr[0];
+            minmax.min = arr[1];
+        } else {
+            minmax.max = arr[1];
+            minmax.min = arr[0];
+        }
+ 
+        for (i = 2; i < n; i++) {
+            if (arr[i] > minmax.max) {
+                minmax.max = arr[i];
+            } else if (arr[i] < minmax.min) {
+                minmax.min = arr[i];
+            }
+        }
+ 
+        return minmax;
+    }
+  
+    
+    minmax = getMinMax(arr, arr_size)
+    console.log("minimum: " + minmax.min,  " maximum " + minmax.max, )
