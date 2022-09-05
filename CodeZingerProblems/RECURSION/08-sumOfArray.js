b703e6c57ff87dea4b38c0611408e9cd98c555e8
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-05 22:15:20 +0530

    sumOfArray

---
+++ CodeZingerProblems/RECURSION/08-sumOfArray.js
@@ -0,0 +1,39 @@
+// Sum of array element using recursion-->
+
+// Find sum of array elements using recursion
+// Given an array, find the sum of array elements using recursion. 
+ 
+
+// Write a function that accepts an array A and size of an array N. The function should return the sum of the array elements. 
+
+
+// Input 
+//     5 
+//     1 2 3 4 5 
+
+
+//     Where, 
+
+// First line of input represents the size of an array. 
+// Second line repsents an array elements. 
+
+
+
+// Solution-->
+
+// function sumOfArray(arr) {
+
+//     let sum = 0
+//     for(let i = 0; i<arr.length; i++) {
+//         sum = sum + arr[i]
+//     }
+//     return sum
+// }
+// let arr = [1, 2, 3, 4, 5];
+// console.log(sumOfArray(arr));
+
+// =============================================
+
+function sumOfArrayUsingRecursion(arr) {
+    
+}
