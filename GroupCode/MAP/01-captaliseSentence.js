23c550c899a0f74f50b31942b3c78ad1a5225046
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-05 16:19:20 +0530

    captaliseFn

--- GroupCode/MAP/01-captaliseSentence.js
+++ GroupCode/MAP/01-captaliseSentence.js
@@ -2,8 +2,8 @@
 // Captalise the first character of eacah word-->
 
 function captaliseFn(arr) {
-    
-let result = []
+
+    let result = []
 
     if (arr.length == 0) {
         return []
@@ -11,9 +11,9 @@ let result = []
 
     // pull out arr[0] and captalise 1st character of arr[0].. "abc" = "A" + "bc" => "Abc"
 
-    let s = arr[0] [0].toUpperCase() + arr[0].slice(1)
+    let s = arr[0][0].toUpperCase() + arr[0].slice(1)
     result.push(s); // ["Abc"]
-   
+
     return result.concat(captaliseFn(arr.slice(1)));
 }
 let arr = ["abc", "pqr", "xylshfhggslkdj", "ravi"]
