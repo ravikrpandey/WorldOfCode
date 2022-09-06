d599770f8af8584f06ba3931099f36a88844f365
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-06 16:20:55 +0530

    updated

---
+++ CodeZingerProblems/NUMBER/swapNumber.js
@@ -0,0 +1,24 @@
+// Swap two number-->
+
+
+let a = 10, b = 20, temp = 0;
+
+temp = a
+a = b
+b = temp
+
+console.log(`a=> ${a} , b=>${b}`);
+
+
+// ========================================
+
+
+// SWAP A NUMBER WITHOUT CREATING 3RD VARIABLE:-
+
+let A = 10, B = 20;
+
+A  = A + B
+B = A - B
+A = A - B
+
+console.log(`A=> ${A}, B=> ${B}`);
