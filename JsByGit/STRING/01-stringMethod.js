59f90773c705a10d27b32e81d14aecb2c47f8503
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-07 22:42:35 +0530

    updated

--- JsByGit/STRING/01-stringMethod.js
+++ JsByGit/STRING/01-stringMethod.js
@@ -219,3 +219,13 @@ console.log(fullName.trim())
 
 // ============================================
 
+// 16. search() ==> it takes a substring as an argument and it returns the index of the first match. The search value can be a string or a regular expression pattern.
+
+// string.search(substring)
+
+// let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+
+// console.log(string.search('love'))          // 2
+// console.log(string.search(/javascript/gi))  // 7
+
+// ============================================
