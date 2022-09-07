07aee598124e74faa76a2cc5a7d98e9416e71218
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-07 23:01:02 +0530

    updated

--- JsByGit/STRING/01-stringMethod.js
+++ JsByGit/STRING/01-stringMethod.js
@@ -264,3 +264,87 @@ console.log(fullName.trim())
 // console.log(txt.match(/\d+/g)) // ["2019", "30", "2020"]
 
 // =============================================
+
+// 18. repeat() ==> it takes a number as argument and it returns the repeated version of the string.
+
+// string.repeat(n)
+
+// let string = 'love'
+// console.log(string.repeat(10)) // lovelovelovelovelovelovelovelovelovelove
+
+// ==============================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Changing Data Type (Casting) ==>
+// Casting: Converting one data type to another data type. We use parseInt(), parseFloat(), Number(), + sign, str() When we do arithmetic operations string numbers should be first converted to integer or float if not it returns an error.
+
+// String to Int ==>
+
+// We can convert string number to a number. Any number inside a quote is a string number. An example of a string number: '10', '5', etc. We can convert string to number using the following methods:
+
+// parseInt()
+// Number()
+// Plus sign(+)
+
+
+// let num = '10'
+// let numInt = parseInt(num)
+// console.log(numInt) // 10
+// let num = '10'
+// let numInt = Number(num)
+
+// console.log(numInt) // 10
+// let num = '10'
+// let numInt = +num
+
+// console.log(numInt) // 10
+
+// =========================================================
+
+
+// String to Float ==>
+// We can convert string float number to a float number. Any float number inside a quote is a string float number. An example of a string float number: '9.81', '3.14', '1.44', etc. We can convert string float to number using the following methods:
+
+// parseFloat() ==>
+
+// Number()
+// Plus sign(+)
+
+// let num = '9.81'
+// let numFloat = parseFloat(num)
+
+// console.log(numFloat) // 9.81
+// let num = '9.81'
+// let numFloat = Number(num)
+
+// console.log(numFloat) // 9.81
+// let num = '9.81'
+// let numFloat = +num
+
+// console.log(numFloat) // 9.81
+
+// =========================================================
+
+// Float to Int ==>
+
+// We can convert float numbers to integers. We use the following method to convert float to int:
+
+// parseInt()==>
+
+// let num = 9.81
+// let numInt = parseInt(num)
+
+// console.log(numInt) // 9
