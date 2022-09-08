91365d40b0637dc9acf17089b920b77d36e0ba12
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-08 23:08:12 +0530

    updated

---
+++ GroupCode/ARRAY/11-removeDublicates.js
@@ -0,0 +1,10 @@
+// Given an array [2, 5, 4, 5, 6, 7, 6, 5, 7, 8, 65, 34] => here we have to remove the diblicates elements and log the unique elements.
+
+function checkUnique(arr) {
+
+    let set = new Set(arr)
+    console.log(set);
+    // return [...set]
+}
+
+checkUnique([2, 5, 4, 5, 6, 7, 6, 5, 7, 8, 65, 34]);
