77093367af08b278c32f6f8227bf97e1632de635
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-09 02:12:48 +0530

    updated

--- GroupCode/ARRAY/13-IsValidNumber.js
+++ GroupCode/ARRAY/13-IsValidNumber.js
@@ -2,16 +2,24 @@
 
 function isValidNumber(input) {
 
+   
     for(let i=0; i<input.length; i++) {
-
+         
+         
+    
         if (input[i] >= 0 && input[i] >= 9) {
-            break;
-        } else {
+
+
+            return "valid number"
+
+        }
+
+        else {
             return "not a valid number"
         }
+    
     }
-
-   
 }
 
-console.log(isValidNumber([1, 5, 7, 4, 6, 3, 7, 2, 8]))
+console.log(isValidNumber(["157463729"]));
+console.log(isValidNumber(["157463o729"]));
