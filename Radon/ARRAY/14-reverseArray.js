50db92b69709f24b21e1bcf45350281911cafbaf
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-15 12:20:35 +0530

    updated

---
+++ Radon/ARRAY/14-reverseArray.js
@@ -0,0 +1,16 @@
+// Reverse the given array-->
+
+function reverseArray(arr) {
+
+    let result ;
+
+    for (let i = arr.length; i>=0; i--) {
+        result = arr[i];
+        console.log(result);
+    }
+    
+}
+
+let arr = "ravi is";
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+reverseArray(arr);
--- Radon/CompanyProblems/FEXLE/cube.js
+++ Radon/CompanyProblems/FEXLE/cube.js
@@ -14,37 +14,37 @@
 
 
 
-#include <iostream>
-#include <algorithm>
-using namespace std;
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
 
-int main() {
-    string str = "you shall not pass";  
-    cout << "Original string: " << str << endl;
+// int main() {
+//     string str = "you shall not pass";  
+//     cout << "Original string: " << str << endl;
 
-    // Use the built-in reverse to get char-by-char reversal.
-    reverse(str.begin(), str.end()); 
+//     // Use the built-in reverse to get char-by-char reversal.
+//     reverse(str.begin(), str.end()); 
 
-    string buffer = "";
-    string ans = "";
+//     string buffer = "";
+//     string ans = "";
 
-    // This for loop then reverses each individual
-    // word in the string.
-    for (int i=0; i < str.length(); i++)
-    {
-      if (str[i] != ' ')
-      {
-        buffer += str[i];
-      }
-      else
-      {
-        reverse(buffer.begin(), buffer.end());
-        ans += buffer + " ";
-        buffer = "";
-      }
-    }
-    // Reversing the last word in the string outside the loop:
-    reverse(buffer.begin(), buffer.end());
-    ans += buffer;
-    cout << "Reversed string: " << ans << endl;
-}
+//     // This for loop then reverses each individual
+//     // word in the string.
+//     for (int i=0; i < str.length(); i++)
+//     {
+//       if (str[i] != ' ')
+//       {
+//         buffer += str[i];
+//       }
+//       else
+//       {
+//         reverse(buffer.begin(), buffer.end());
+//         ans += buffer + " ";
+//         buffer = "";
+//       }
+//     }
+//     // Reversing the last word in the string outside the loop:
+//     reverse(buffer.begin(), buffer.end());
+//     ans += buffer;
+//     cout << "Reversed string: " << ans << endl;
+// }
