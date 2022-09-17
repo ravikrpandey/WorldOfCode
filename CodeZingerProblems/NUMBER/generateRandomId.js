6a114d44ab8a69379ed5f464c07fba17b8f680e6
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-17 23:10:11 +0530

    updated

--- CodeZingerProblems/NUMBER/generateRandomId.js
+++ CodeZingerProblems/NUMBER/generateRandomId.js
@@ -1,9 +1,16 @@
 // How to generate random id-->
 
-function generateId() {
+// function generateId() {
        
-    return Math.random().toString(36).substring(2) +
-      (new Date()).getTime().toString(36);
-  }
+//     return Math.random().toString(36).substring(2) +
+//       (new Date()).getTime().toString(36);
+//   }
   
-  console.log(generateId());
+//   console.log(generateId());
+
+//===========================Or======================
+
+function uniqueID() {
+  return Math.floor(Math.random() * Date.now())
+  }
+  console.log(uniqueID());
