cf3d4263519307ba9d7272c5794fe15f4c95e3a5
ravikrpandey <ravi.pandey.iet@gmail.com>
2022-09-21 12:21:07 +0530

    updated

---
+++ JsByAkshaySaini/function/howFunctionWorks.js
@@ -0,0 +1,21 @@
+
+// working of function
+
+let x = 10
+
+function a(){
+
+    let x = 15
+    console.log(x)
+}
+
+function b() {
+
+    let x = 20
+    console.log(x)
+}
+
+a()
+b()
+
+console.log(x)
---
+++ JsByAkshaySaini/hoisting/hoisting.js
@@ -0,0 +1,14 @@
+
+// console.log(a * a)
+
+let a = 5;
+
+nameFunction(10)
+function nameFunction (num) {
+
+   let sum = num * num
+
+   console.log(sum)
+
+}
+
